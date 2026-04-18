cc86762d0d742325e6057a2dd0f709bd98b2e1df
sa-y-em <sayemmir100@gmail.com>
2026-04-19 00:03:30 +0600

    flag project started

---
+++ components/Countries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            <h1>Get started with flag</h1>
+        </div>
+    );
+};
+
+export default Countries;
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+import Countries from '../components/countries'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        
+        <div>
+          <Countries></Countries>
+        </div>
+      </section>
+
+    </>
+  )
+}
+
+export default App
